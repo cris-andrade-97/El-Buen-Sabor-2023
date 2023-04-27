0a870a262c7bbe8d908e6778479a3ceb0823eed4
FrancoIvanMoreno14 <81662831+FrancoIvanMoreno14@users.noreply.github.com>
2023-04-27 20:30:03 -0300

    commit

--- frontend/el-buen-sabor-frontend/src/app/components/rubros/formulario-rubro-ingredientes/formulario-rubro-ingredientes.component.ts
+++ frontend/el-buen-sabor-frontend/src/app/components/rubros/formulario-rubro-ingredientes/formulario-rubro-ingredientes.component.ts
@@ -1,53 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
-import { UntypedFormGroup, UntypedFormBuilder, UntypedFormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {
+  UntypedFormGroup,
+  UntypedFormBuilder,
+  UntypedFormControl,
+} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import RubroIngredientesJSON from '../grilla-rubro-ingredientes/RubroIngredientes.json'
+import RubroIngredientesJSON from '../grilla-rubro-ingredientes/RubroIngredientes.json';
 
 //import { lecturaDatos } from 'persistencia/persistencia';
 
 @Component({
   selector: 'app-formulario-rubro-ingredientes',
   templateUrl: './formulario-rubro-ingredientes.component.html',
-  styleUrls: ['./formulario-rubro-ingredientes.component.css']
+  styleUrls: ['./formulario-rubro-ingredientes.component.css'],
 })
 export class FormularioRubroIngredientesComponent implements OnInit {
-
-  rubro!:UntypedFormGroup;  
+  rubro!: UntypedFormGroup;
   esNuevo: boolean = false;
   rubroIngrediente!: any;
-  nombre = new UntypedFormControl("")
+  nombre: string = '';
   estado: boolean = false;
 
   id = this.route.snapshot.paramMap.get('id');
 
-  constructor(private route: ActivatedRoute,private fb: UntypedFormBuilder) { }
+  constructor(private route: ActivatedRoute, private fb: UntypedFormBuilder) {}
 
-  ngOnInit(): void {  
-    
-    this.rubroIngrediente = RubroIngredientesJSON["Ingredientes_No_Vendibles"][Number(this.id)];  
+  async ngOnInit(): Promise<void> {
+    //Obtengo Rubro
+    this.rubroIngrediente =
+      RubroIngredientesJSON['Ingredientes_No_Vendibles'][Number(this.id)];
 
-    this.estado = this.rubroIngrediente.estado; 
-    this.nombre.setValue (this.rubroIngrediente.nombre)
-    if(this.id == 'nuevoRubro'){
+    //Si es nuevo deja el formulario en blanco
+    if (this.id == 'nuevoRubro') {
       this.esNuevo = true;
+    } else {
+      //Si es modificación seteo los valores
+      this.estado = this.rubroIngrediente.estado;
+      this.nombre = this.rubroIngrediente.nombre;
     }
-    
-
   }
 
-  asignarEstado(estadoRubro: boolean){
-    this.estado = estadoRubro;     
+  validaSeVende() {
+    if (this.estado == true) {
+      this.estado = true;
+      return true;
+    }
+    return false;
   }
 
-  onSubmit(){
+  asignarEstado(estadoRubro: boolean) {
+    this.estado = estadoRubro;
+  }
+
+  onSubmit() {
     //lecturaDatos(Number(this.id),this.nombre.value,this.estado)
-    
 
-    
-
-
-    window.location.replace("/grilla-rubro-ingredientes")
+    window.location.replace('/grilla-rubro-ingredientes');
   }
-  
 }
