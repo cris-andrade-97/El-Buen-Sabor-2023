fa039c54d2656a512fdf6c67108e55fad01dfb10
cris-andrade-97 <cristian201097@gmail.com>
2023-05-01 18:01:24 -0300

    acceso y grilla ingredientes funcional, ver loader

--- frontend/el-buen-sabor-frontend/src/app/components/ingredientes/grilla-ingredientes/grilla-ingredientes.component.ts
+++ frontend/el-buen-sabor-frontend/src/app/components/ingredientes/grilla-ingredientes/grilla-ingredientes.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-grilla-ingredientes',
   templateUrl: './grilla-ingredientes.component.html',
   styleUrls: ['./grilla-ingredientes.component.css']
 })
-export class GrillaIngredientesComponent {
 
+export class GrillaIngredientesComponent implements OnInit {
+
+  ingredientes!: any;
+
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService) { }
+
+  async ngOnInit() {
+    await this.llenarLista()
+  }
+
+  async llenarLista() {
+    let url = 'http://localhost:3000/api/ingredientes/listar';
+
+    this.http.get(url).subscribe((response) => {
+      this.ingredientes = response;
+    });
+  }
+
+  actualizarVigencia(id: number, estado: boolean) {
+    this.spinner.show();
+    let url =
+      'http://localhost:3000/api/ingredientes/modificar-estado/' + id;
+
+    this.http
+      .put(url, {
+        estado: estado,
+      })
+      .subscribe((response) => console.log(response));
+
+    window.location.reload();
+  }
 }
--- frontend/el-buen-sabor-frontend/src/app/components/rubros/grilla-rubro-ingredientes/grilla-rubro-ingredientes.component.ts
+++ frontend/el-buen-sabor-frontend/src/app/components/rubros/grilla-rubro-ingredientes/grilla-rubro-ingredientes.component.ts
@@ -9,27 +9,24 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class GrillaRubroIngredientesComponent implements OnInit {
   rubrosIngredientes: any;
 
-  constructor(private http: HttpClient, private spinner: NgxSpinnerService) {}
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService) { }
 
   async ngOnInit(): Promise<void> {
     await this.llenarLista();
   }
 
   async llenarLista() {
-    
     let url = 'http://localhost:3000/api/rubro-ingredientes/listar';
 
     this.http.get(url).subscribe((response) => {
       this.rubrosIngredientes = response;
     });
-    
   }
 
   actualizarVigencia(id: number, estado: boolean) {
     this.spinner.show();
     let url =
-      'http://localhost:3000/api/rubro-ingredientes/modificar-estado-rubro/' +
-      id;
+      'http://localhost:3000/api/rubro-ingredientes/modificar-estado-rubro/' + id;
 
     this.http
       .put(url, {
